144b4b7dc7db660f1acbe004a80b671ae20399b8
atiyehp <42898707+atiyehp@users.noreply.github.com>
2022-05-24 00:16:02 +0430

    Add files via upload
    1st commit

---
+++ model_paths.py
@@ -0,0 +1,14 @@
+import joblib
+import pickle
+
+#if 'google.colab' in str(get_ipython()):
+#    model_dir = "AlloyML_Public/models"
+#else:
+#    model_dir = "models"
+model_dir = "models"
+
+models = {"elongation": joblib.load(f"{model_dir}/ductility"),
+          "tensile": joblib.load(f"{model_dir}/UTS"),
+          "yield": joblib.load(f"{model_dir}/YS")
+          }
+
